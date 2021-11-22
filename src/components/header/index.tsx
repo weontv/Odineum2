a03aa41f3b44e8d6b49efa1a38582001f98dae34
GreatWorldDev <great.world.dev@gmail.com>
2021-11-22 13:46:24 +0900

    add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { styles } from "./Header.module.scss";
+import styles from "./Header.module.scss";
 
-function Landing() {
+function Header() {
   return (
     <>
-      <div className={styles.navbar}>
-        
+      <div className={`${styles.navbar} flex items-center`}>
+        <img src="img/logo.png" alt="logo" className={styles.logo} />
+        <ul>
+          <li>NFT</li>
+          <li className="flex items-center">ROADMAP <img src="img/navdropdown.png" alt="drop" className="ml-4"/></li>
+          <li>TOKENOMICS</li>
+          <li>CHART</li>
+          <li>WHITEPAPER</li>
+        </ul>
+        <div className="hidden">
+          <span />
+          <span />
+          <span />
+        </div>
       </div>
     </>
   );
 };
 
-export default Landing;
+export default Header;
