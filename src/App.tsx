751ab2d49c126a9b7552841bb85d1a07f9fe850a
GreatWorldDev <great.world.dev@gmail.com>
2021-11-22 13:45:33 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.scss";
-import Routes from "./routes";
+import Router from "./routes";
 
 function App() {
   return (
     <div className="app">
-      <Routes />
+      <Router />
     </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "../pages/landing";
+import Header from "../components/header";
 
-function Routes() {
+function Router() {
   return (
-    <>
-      <BrowserRouter>
-        <Route exact path="/" component={Landing} />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/">{Landing}</Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default Routes;
+export default Router;
