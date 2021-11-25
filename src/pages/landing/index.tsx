0f23c7f6994709865b4fe6755ffcd57d0acd5afb
GreatWorldDev <great.world.dev@gmail.com>
2021-11-26 01:05:13 +0900

    finish project

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import Header from "../../components/header";
+import Banner from "../../components/banner";
+import Nft from "../../components/nft";
+import RoadMap from "../../components/roadmap";
+import Tokenomic from "../../components/tokenomics";
+import Chart from "../../components/chart";
 
 function Landing() {
   return (
     <>
-      <Header />
+      <Banner />
+      <Nft />
+      <RoadMap />
+      <Tokenomic />
+      <Chart />
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from "../pages/landing";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/">{Landing}</Route>
+        <Route path="/" element={<Landing />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
