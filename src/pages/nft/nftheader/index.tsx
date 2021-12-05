a26ab07bede98cf3739abbdfbabafecff6389cbb
GreatWorldDev <great.world.dev@gmail.com>
2021-12-06 04:52:23 +0900

    add nft page header

--- src/pages/nft/nftheader/index.tsx
+++ src/pages/nft/nftheader/index.tsx
@@ -1,37 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { CustomDropdown } from "../../../components/ui/customDropdown";
+import CustomDropdown from "../../../components/ui/customDropdown";
 import styles from "./NftHeader.module.scss";
-
-const languageList = [
-  'ENG',
-  'SPAIN',
-  'CHINESE'
-];
 
 const priceType = [
   'USD',
-  'ETH'
+  'BNB'
 ];
 
 function NftHeader() {
   const [isHamburger, setIsHamburger] = useState<boolean>(false);
-  const [language, setLanguage] = useState<string>(languageList[0]);
   const [price, setPrice] = useState<string>(priceType[0]);
   return (
     <>
       <div className={styles.navbar}>
-        <div className="flex">
-          <div className="flex items-center z-10">
+        <div className="flex items center">
+          <div className="flex items-center z-10 mr-8">
             <Link to="/">
               <img src="img/logo.png" alt="logo" className={styles.logo} />
             </Link>
             <h1 className="ml-4"><span>NFT</span> MARKETPLACE</h1>
           </div>
-          <div className="flex items-center">
-            <CustomDropdown selected={language} lists={languageList} handleSelect={(item) => setLanguage(item)} classProps="mx-4" />
-            <CustomDropdown selected={price} lists={priceType} handleSelect={(item) => setPrice(item)} />
-          </div>
+          <CustomDropdown selected={price} lists={priceType} handleSelect={(item) => setPrice(item)} />
         </div>
         <div className="flex items-center">
           <button type="button" className={styles.walletBtn}>CONNECT WALLET</button>
